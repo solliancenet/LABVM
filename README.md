6a37ae9d4a8b40d4d82f3bc03f5aaac7c478577a
Dan Patrick <dan@clouddeltav.com>
2018-04-07 07:28:02 -0400

    LABVM

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# LABVM
-Install Lab VMs
+# AzureNestedHyperV
+Create a Nested HyperV Host in Azure with one click
+
+<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fdeltadan%2Fazurenestedhyperv%2Fmaster%2Fazure-deploy.json" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
---
+++ customscripts/iotlab.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+$Packages = 'googlechrome',`
+            'git.install',`
+            'docker-for-windows',`
+            'postman',`
+            'nodejs.install',`
+            'visualstudiocode'
+
+ForEach ($PackageName in $Packages)
+{choco install $PackageName -y}
+
+Add-LocalGroupMember -Member demouser -Group docker-users
+Restart-Computer
+
